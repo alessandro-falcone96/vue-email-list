c0973f719ad0b9fe53df57f84389554151053c58
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-04-07 18:09:05 +0200

    Completato esercizio

--- js/script.js
+++ js/script.js
@@ -3,14 +3,28 @@ var app = new Vue(
     el: "#app",
     data: {
       email: "",
+      listaEmail: []
     },
     methods: {
-      generateEmail: function() {
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then((risposta) => {
-          this.email = risposta.data.response;
-        });
-      },
-    }
+      generaEmail: function() {
+        this.listaEmail = [];
+        for (var i = 0; i < 10; i++) {
+          axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+          .then((risposta) => {
+            this.email = risposta.data.response;
+            this.listaEmail.push(this.email);
+          });
+        };
+      }
+    },
+    // mounted: function() {
+    //   for (var i = 0; i < 10; i++) {
+    //     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    //     .then((risposta) => {
+    //       this.email = risposta.data.response;
+    //       this.listaEmail.push(this.email);
+    //     });
+    //   };
+    // }
   }
 );

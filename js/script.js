8d06610e0d6c29b42fb90f275fb97b5c1212d003
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-04-07 17:56:30 +0200

    Aggiunta funzione

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-// Inserisci il codice qui
+var app = new Vue(
+  {
+    el: "#app",
+    data: {
+      email: "",
+    },
+    methods: {
+      generateEmail: function() {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((risposta) => {
+          this.email = risposta.data.response;
+        });
+      },
+    }
+  }
+);

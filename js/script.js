2b3c461a7030074ec25a0b03aaf6f8b21b222624
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-04-07 18:23:21 +0200

    Aggiunto un input

--- js/script.js
+++ js/script.js
@@ -3,18 +3,20 @@ var app = new Vue(
     el: "#app",
     data: {
       email: "",
-      listaEmail: []
+      listaEmail: [],
+      numero: ""
     },
     methods: {
       generaEmail: function() {
         this.listaEmail = [];
-        for (var i = 0; i < 10; i++) {
+        for (var i = 0; i < this.numero; i++) {
           axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
           .then((risposta) => {
             this.email = risposta.data.response;
             this.listaEmail.push(this.email);
           });
         };
+        this.numero = "";
       }
     },
     // mounted: function() {
